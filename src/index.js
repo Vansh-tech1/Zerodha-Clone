5b2fe60dcf3fa346252112f067e72ad7215b4cff
Vansh <sainivansh765@gmail.com>
2025-03-30 19:05:52 +0530

    created basic stucture

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
+import Navbar from './landing_page/Navbar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -8,6 +9,7 @@ root.render(
   <React.StrictMode>
     <div>
       <h1>Hello</h1>
+      < Navbar />
     </div>
   </React.StrictMode>
 );
